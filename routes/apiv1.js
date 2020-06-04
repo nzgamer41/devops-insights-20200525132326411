a9b3e771b2395267fd8f96e8b8d8827b70f9bf68
Alexander Cheer <arc37@students.waikato.ac.nz>
2020-06-04 12:40:40 +0000

    fixed testing stuff

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -15,7 +15,7 @@ exports.getWeather = function(req, res) {
 		return res.status(400).send('zip missing');
 	}
 
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	var aurl = OPENWEATHERURL + '&q=' + zip + ',nz';
 
 	request({
 		method: 'GET',
@@ -27,7 +27,7 @@ exports.getWeather = function(req, res) {
     		//console.error("Failed to send request to openweathermap.org", err);
     	} else {
     		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
     			var response = {city: body.name, weather: weath};
     			return res.status(200).send(response);
     		} else {
@@ -76,7 +76,7 @@ exports.getWeather3 = function(req, res) {
 		return res.status(400).send('zip missing');
 	}
 
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	var aurl = OPENWEATHERURL + '&q=' + zip + ',nz';
 
 	request({
 		method: 'GET',
@@ -88,7 +88,7 @@ exports.getWeather3 = function(req, res) {
     		//console.error("Failed to send request to openweathermap.org", err);
     	} else {
     		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
     			var response = {city: body.name, weather: weath};
     			return res.status(200).send(response);
     		} else {
--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -23,7 +23,7 @@ sinon.spy(resMock, "send");
 
 describe('Get Weather', function() {
 
-  it('with without zip code', function() {
+  it('with without City Name', function() {
     reqMock = {
       query: {
 
@@ -35,10 +35,10 @@ describe('Get Weather', function() {
     assert(resMock.status.lastCall.calledWith(400), 'Unexpected status code:' + resMock.status.lastCall.args);
   });
 
-  it('with valid zip code and error from request call', function() {
+  it('with valid City Name and error from request call', function() {
     reqMock = {
       query: {
-        zip: 79968
+        zip: 'Palmerston North'
       }
     };
 
@@ -54,10 +54,10 @@ describe('Get Weather', function() {
     assert(resMock.send.lastCall.calledWith('Failed to get the data'), 'Unexpected response:' + resMock.send.lastCall.args);
   });
 
-  it('with incomplete zip code', function() {
+  it('with incomplete city name', function() {
     reqMock = {
       query: {
-        zip: 79968
+        zip: 'Palmer'
       }
     };
 
@@ -76,20 +76,20 @@ describe('Get Weather', function() {
   it('with valid zip code', function() {
     reqMock = {
       query: {
-        zip: 79968
+        zip: 'Palmerston North'
       }
     };
 
     const body = {
       cod: 200,
-      name: 'El Paso',
+      name: 'Palmerston North',
       weather: [
         {
           main: 'cold'
         }
       ],
       main: {
-        temp: 78
+        temp: 10.47
       }
     };
 
@@ -102,7 +102,7 @@ describe('Get Weather', function() {
     apiv1.getWeather(reqMock, resMock);
 
     assert(resMock.status.lastCall.calledWith(200), 'Unexpected response:' + resMock.status.lastCall.args);
-    assert(resMock.send.lastCall.args[0].city === 'El Paso', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
-    assert(resMock.send.lastCall.args[0].weather === 'Conditions are cold and temperature is 78 F', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
+    assert(resMock.send.lastCall.args[0].city === 'Palmerston North', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
+    assert(resMock.send.lastCall.args[0].weather === 'Conditions are cold and temperature is 10.47 C', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
   });
 });
